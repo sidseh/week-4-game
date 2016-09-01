93552f421f05422b6e9de84a7823cd71f1edca0f
siddhant <siddhant@siddhants-MacBook-Air.local>
2016-09-01 17:10:20 -0400

    sports

---
+++ assets/javascript/game.js
@@ -0,0 +1,46 @@
+
+
+
+var balls ={
+	basektball:
+	{
+		name: "basektball",
+		value: 0
+
+	},
+	cricket: {
+		name: "cricket",
+		value: 0
+	},
+	football: {
+		name: "football",
+		value: 0
+	},
+	golf: {
+		name: "golf",
+		value: 0
+	}
+};
+
+
+
+var score =  0;
+var targetScore = 0;
+
+
+
+$("#basektball").click(function() {
+	
+});
+
+$("#cricket").click(function() {
+
+});
+
+$("#football").click(function() {
+
+});
+
+$("#golf").click(function() {
+
+});
